63e27a75a399719dc5766b52d5d2b2ed1173542c
riizu <rgbatty@outlook.com>
2016-08-02 10:35:30 -0600

    Adjust SearchService spec to expect an instance to be made, rather than self methods

--- spec/services/search_spec.rb
+++ spec/services/search_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe SearchService, :vcr do
+  it "has a base connection" do
+    ss = SearchService.new
+
+    url_prefix = ss.connection.url_prefix.to_s
+    api_key = ss.connection.params["api_key"]
+
+    expect(url_prefix).to eq "https://api.bestbuy.com/"
+    expect(api_key).to eq ENV["BEST_BY"]
+  end
+
   it "searches for stores by zip" do
-    result = SearchService.find_stores('80202')
+    ss = SearchService.new
+
+    result = ss.find_stores('80202')
 
     expect(result["from"]).to eq 1
     expect(result["to"]).to eq 15
