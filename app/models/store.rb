3c906f003be8f23cb29b4f65f2ba34357f78f958
riizu <rgbatty@outlook.com>
2016-08-02 12:04:17 -0600

    Final commit

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,6 +1,7 @@
 class Store
-  attr_reader :long_name, :city, :distance, :phone, :type, :id
-  
+  attr_reader :long_name, :city, :distance, :phone, :type, :id, :hours,
+              :zip, :region
+
   def initialize(store)
     @long_name = store["longName"]
     @city = store["city"]
@@ -8,5 +9,19 @@ class Store
     @phone = store["phone"]
     @type = store["storeType"]
     @id = store["storeId"]
+    @hours = make_hours(store["hoursAmPm"])
+    @address = make_address(store["address"])
+    @region = store["region"]
+    @zip = store["postalCode"]
+  end
+
+  def make_hours(hours)
+    if hours != nil
+      hours.split(";")
+    end
+  end
+
+  def make_address(address)
+    "#{address}, #{city}, #{region} #{zip}"
   end
 end
