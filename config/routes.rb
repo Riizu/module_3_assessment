35a11af5513674327e476163a093ee78130869ca
riizu <rgbatty@outlook.com>
2016-08-02 11:35:26 -0600

    Adjust results page to link to individual search result shows

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :items,  only: [:index, :show]
   resources :orders, only: [:index, :show]
   resources :users,  only: [:index, :show]
-  resources :search,  only: [:index]
+  resources :search,  only: [:index, :show]
 
   namespace 'api' do
     namespace 'v1' do
