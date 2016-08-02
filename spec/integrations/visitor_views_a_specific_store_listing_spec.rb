b0f4fbe36c0380f6cb1ecad79634de8e825dafba
riizu <rgbatty@outlook.com>
2016-08-02 11:27:37 -0600

    Add integraton test for store search result show page

---
+++ spec/integrations/visitor_views_a_specific_store_listing_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature 'A visitor can view a specific store search result' do
+  context 'valid' do
+    scenario 'They have searched for a set of results', :vcr do
+      visit '/zip?80202'
+      click_on 'Best Buy Mobile - Cherry Creek Shopping Center'
+
+      expect(current_path).to eq '/stores/1'
+
+      expect(page).to have_content "Best Buy Mobile - Cherry Creek Shopping Center"
+      expect(page).to have_content "Mobile"
+      expect(page).to have_content "Denver"
+      expect(page).to have_content "3000 East First Ave, Denver, CO 80206"
+
+      within ".schedule" do
+        expect(page).to have_content("Mon: 10am-9pm")
+        expect(page).to have_content("Tue: 10am-9pm")
+        expect(page).to have_content("Wed: 10am-9pm")
+        expect(page).to have_content("Thurs: 10am-9pm")
+        expect(page).to have_content("Fri: 10am-9pm")
+        expect(page).to have_content("Sat: 10am-9pm")
+        expect(page).to have_content("Sun: 11am-7pm")
+      end
+
+    end
+  end
+end
